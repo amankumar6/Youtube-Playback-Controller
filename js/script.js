99a07b944e4477df2aac3c3b164697ea46d721fc
amankumar6 <amankumar786dpsdh@gmail.com>
2022-01-09 20:58:52 +0530

    fix: loading issue

--- js/script.js
+++ js/script.js
@@ -2,22 +2,47 @@ chrome.runtime.sendMessage({todo: "showPageAction"});
 
 const playBack = [1, 1.1, 1.15, 1.2, 1.30, 1.35, 1.40, 1.45, 1.55, 1.65, 1.85, 1.95, 2.25, 2.50, 2.75, 3];
 
-$( document ).ready(function() {
-    setTimeout(() => {
-        $("#meta-contents").before(`<div class="selector-div"><select class="selector" aria-label="Default select example"></select></div>`);
+// $( document ).ready(function() {
+//     setTimeout(() => {
+//         $("#meta-contents").before(`<div class="selector-div"><select class="selector" aria-label="Default select example"></select></div>`);
 
-        $(".selector").html(playBack.map((e) => `<option value="${e}"> x ${e} </option>`).join(''));
+//         $(".selector").html(playBack.map((e) => `<option value="${e}"> x ${e} </option>`).join(''));
 
-        chrome.storage.sync.get(['playbackRate'], (result) => {
-            $("selector").val(result.playbackRate);
-            document.getElementsByClassName('html5-main-video')[0].playbackRate = result.playbackRate;
-        });
+//         chrome.storage.sync.get(['playbackRate'], (result) => {
+//             $("selector").val(result.playbackRate);
+//             document.getElementsByClassName('html5-main-video')[0].playbackRate = result.playbackRate;
+//         });
         
-        $(".selector").change((e) => {
-            if(e.target.value){
-                document.getElementsByClassName('html5-main-video')[0].playbackRate = e.target.value;
-                chrome.storage.sync.set({playbackRate: e.target.value});
-            } 
-        });
-    }, 1000);
+//         $(".selector").change((e) => {
+//             if(e.target.value){
+//                 document.getElementsByClassName('html5-main-video')[0].playbackRate = e.target.value;
+//                 chrome.storage.sync.set({playbackRate: e.target.value});
+//             } 
+//         });
+//     }, 1000);
+// });
+
+$( document ).ready(function() {
+    let flag = 0;
+    let timeout = setInterval(() => {
+        if(document.querySelector("#meta-contents") && flag == 0){
+            $("#meta-contents").before(`<div class="selector-div"><select class="selector" aria-label="Default select example"></select></div>`);
+    
+            $(".selector").html(playBack.map((e) => `<option value="${e}"> x ${e} </option>`).join(''));
+    
+            chrome.storage.sync.get(['playbackRate'], (result) => {
+                $("selector").val(result.playbackRate);
+                document.getElementsByClassName('html5-main-video')[0].playbackRate = result.playbackRate;
+            });
+            
+            $(".selector").change((e) => {
+                if(e.target.value){
+                    document.getElementsByClassName('html5-main-video')[0].playbackRate = e.target.value;
+                    chrome.storage.sync.set({playbackRate: e.target.value});
+                } 
+            });
+            flag = 1;
+        }
+        if(flag == 1) clearInterval(timeout);
+    }, 1);
 });
